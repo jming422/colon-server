88ba8e0374e5bcbeddfb4345bae6548203e33a16
Jonathan Ming <jming@pkcsecurity.com>
2019-11-22 14:05:27 -0800

    Update CORS settings

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Koa = require('koa');
 const Router = require('koa-router');
 const BodyParser = require('koa-bodyparser');
 const respond = require('koa-respond');
+const Cors = require('@koa/cors');
 
 const router = require('./routes');
 
@@ -9,6 +10,17 @@ const db = require('./db');
 db.initDb();
 
 const app = new Koa();
+app.use(
+  Cors({
+    origin: (ctx) => {
+      if (ctx.url === '/api/colon') {
+        return '*';
+      } else {
+        return ctx.get('Origin');
+      }
+    },
+  })
+);
 app.use(BodyParser());
 app.use(respond({ autoMessage: false }));
 
